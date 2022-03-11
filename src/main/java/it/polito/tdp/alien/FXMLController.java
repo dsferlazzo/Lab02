40939bfaae7774c18dade10bf77a1dc0ed47550c
Daniele Sferlazzo <S272348@studenti.polito.it>
2022-03-11 19:20:04 +0100

    Esercizio opzionale

--- src/main/java/it/polito/tdp/alien/FXMLController.java
+++ src/main/java/it/polito/tdp/alien/FXMLController.java
@@ -3,7 +3,6 @@ package it.polito.tdp.alien;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 import it.polito.alien.model.Parola;
 import javafx.event.ActionEvent;
@@ -33,17 +32,37 @@ public class FXMLController {
     	
     	for(int i = 0;i<s.length();i++)
     	{
-    		if(!Character.isAlphabetic(s.charAt(i)) && s.charAt(i)!=' ')
+    		if(!Character.isAlphabetic(s.charAt(i)) && s.charAt(i)!=' ' && s.charAt(i)!='?')
     		{
     			txtArea.setText(txtArea.getText() + "\nFormato Errato");
     			return;
     		}
     			
     	}
+    	int flDomanda=0;
+    	for(int i = 0;i<s.length();i++)
+    	{
+    		if(s.charAt(i)=='?')
+    			flDomanda++;
+    			
+    	}
+    	
+    	if(flDomanda>1)
+    	{
+    		txtArea.setText(txtArea.getText() + "\nFormato Errato");
+			return;
+    	}
     	
     	
     	if(s.contains(" "))
     	{		//GESTISCO INSERIMENTO PAROLA
+    		
+    		if(s.contains("?"))
+    		{
+    			txtArea.setText(txtArea.getText() + "\nFormato Errato");
+    			return;
+    		}
+    		
     		String array[] = s.split(" ");
     		
     		
@@ -61,6 +80,58 @@ public class FXMLController {
     	}
     	
     	//GESTISCO RICHIESTA DI TRADUZIONE
+    	
+    	
+    	//GESTISCO PRESENZA DI '?'
+    	if(s.contains("?"))
+    	{
+    		List<Parola> pTrovate = new ArrayList<Parola>();
+    		for(int i = 0;i<lp.size();i++)
+    		{
+    			if(lp.get(i).getpAliena().length()==s.length())
+    			{
+	    			int flUguaglianza=1;
+	    			String pAliena = lp.get(i).getpAliena();
+	    			for(int j = 0;j<pAliena.length();j++)
+	    			{
+	    				if(pAliena.charAt(j)!=s.charAt(j) && s.charAt(j)!='?')
+	    					flUguaglianza=0;	
+	    			}
+	    			if(flUguaglianza==1)
+	    				pTrovate.add(lp.get(i));	
+    			}	
+    		}
+    		
+    		if(pTrovate.size()==0)
+    		{
+    			txtArea.setText(txtArea.getText() + "\nNessuna traduzione trovata");
+    			return;
+    		}
+    		if(pTrovate.size()==1)
+    		{
+    			List<String> ls1 = new ArrayList<String>(pTrovate.get(0).getpUmana());
+    			for(int j = 0;j<ls1.size();j++)
+    			{
+    				txtArea.setText(txtArea.getText()+ "\n" + ls1.get(j));
+    			}
+    			return;
+    		}
+    		if(pTrovate.size()>1)
+    		{
+    			txtArea.setText(txtArea.getText() + "\nPiù di una parola è stata trovata");
+    			
+    			for(int i = 0;i<pTrovate.size();i++)
+    			{
+    				List<String> ls1 = new ArrayList<String>(pTrovate.get(i).getpUmana());
+        			for(int j = 0;j<ls1.size();j++)
+        			{
+        				txtArea.setText(txtArea.getText()+ "\n" +
+        				pTrovate.get(i).getpUmana() + "///" + ls1.get(j));
+        			}
+    			}
+    			return;
+    		}
+    	}
     	for(int i = 0;i<lp.size();i++)
     	{
     		if(lp.get(i).getpAliena().compareTo(s)==0)
