159d4e87127cff91393f324f42f6442cf26d744a
Daniele Sferlazzo <S272348@studenti.polito.it>
2022-03-16 16:38:30 +0100

    Esercizio opzionale

--- src/main/java/it/polito/alien/model/Dizionario.java
+++ src/main/java/it/polito/alien/model/Dizionario.java
@@ -17,16 +17,28 @@ public class Dizionario {
 		parole.add(p);
 	}
 	
-	public PAliena cercaParola(String parola) {
-		
+	public List<PAliena> cercaParola(String parola) {	//DA MODIFICARE PER IMPLEMENTARE RICERCA CON WILDCARD
+		List<PAliena> output = new ArrayList<PAliena>();
 		for(PAliena pa:parole)
 		{
-			if(pa.getParola().equals(parola))
-				return pa;
+			int pTrovata=0;
+			String s = pa.getParola();
+			if(s.length()==parola.length())
+			{
+				pTrovata=1;
+				for(int i = 0;i<s.length();i++)
+				{
+					if(s.charAt(i)!=parola.charAt(i) && parola.charAt(i)!='?')
+						pTrovata=0;
+				}
+				if(pTrovata==1)
+					output.add(pa);
+			}
 		}
 		
-	
-		return null;		//ECCEZIONE DA GESTIRE IN CONTROLLER
+		if(output.size()>0)
+		return output;		
+		else return null;		//ECCEZIONE DA GESTIRE IN CONTROLLER
 	}
 
 }
--- src/main/java/it/polito/tdp/alien/FXMLController.java
+++ src/main/java/it/polito/tdp/alien/FXMLController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import it.polito.alien.model.Dizionario;
+import it.polito.alien.model.PAliena;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
@@ -31,12 +32,45 @@ public class FXMLController {
     	String s = txtParola.getText().toLowerCase();	//CONVERTITO IN MINUSCOLO
     	txtParola.clear();
     	
+    	//CONTROLLO SUL FORMATO DELLA STRINGA
+    	int flWildCard=0;
+    	for(int i = 0;i<s.length();i++)
+    	{	//CONTROLLO CHE NON CI SIANO CARATTERI NON VALIDI
+    		Character c = s.charAt(i);
+    		if(!Character.isAlphabetic(c) && c!=' ' && c!='?')
+    		{
+    			txtTraduzioni.appendText("ERRORE: formato della stringa inserita errato\n");
+    			return;
+    		}
+    		if(c=='?')
+    			flWildCard++;
+    	}
+    	if(flWildCard>1)
+    	{	//NON POSSO RICHIEDERE TRADUZIONE DI PAROLA CON PIU DI UN '?'
+    		txtTraduzioni.appendText("ERRORE: formato della stringa inserita errato\n");
+			return;
+    	}
+    	if(s.contains("?") && s.contains(" "))
+    	{	//NON POSSO INSERIRE PAROLA CON ? NEL DIZIONARIO
+    		txtTraduzioni.appendText("ERRORE: formato della stringa inserita errato\n");
+			return;
+    	}
+    	
+    	
+    	
     	String array[] = s.split(" ");
     	
-    	if(array.length==1)	//GESTISCI TRADUZIONE
+    	if(array.length==1)	//GESTISCI TRADUZIONE, DA MODIFICARE CAUSA AGGIUNTA WILDCARD
     	{
     		try {
-    		txtTraduzioni.appendText(model.cercaParola(s).toString());
+    			List<PAliena> lpa = model.cercaParola(s);
+    			
+    			for(int i = 0;i<lpa.size();i++)
+    			{
+    				txtTraduzioni.appendText(lpa.get(i).toString());
+    			}
+    			
+    		//txtTraduzioni.appendText(model.cercaParola(s).toString());
     		} catch (NullPointerException npe ) //GESTISCO PAROLA NON TROVATA
     		{
     			txtTraduzioni.appendText("Traduzione non trovata\n");
@@ -45,7 +79,7 @@ public class FXMLController {
     		return;
     	}
     	
-    	//GESTISCI INSERIMENTO PAROLA
+    	//GESTISCI INSERIMENTO PAROLA, IDENTICA A METODO SENZA WILDCARD
     	
     	List<String> trad = new ArrayList<String>();
     	for(int i = 1;i<array.length;i++)
